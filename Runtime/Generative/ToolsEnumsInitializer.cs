7ccb48225d4d43641d899bf30fa2895582c3794d
Bohdan Tsaplia <me@gishadev.com>
2025-07-05 00:05:40 +0300

    add simplified debugging

---
+++ Runtime/Generative/ToolsEnumsInitializer.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace gishadev.tools.Generative
+{
+    [InitializeOnLoad]
+    public static class ToolsEnumsInitializer
+    {
+        static ToolsEnumsInitializer()
+        {
+            Debug.Log("Custom package loaded in Editor!");
+        }
+}
